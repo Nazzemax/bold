26092fecfd8fde960b477ced852c7f662b6b9197
rustamBoss11 <rus112004rus@gmail.com>
2025-01-23 21:25:00 +0600

    reviews

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Carousel from "@/src/components/Carousel/Carousel";
 import Cases from "@/src/components/Cases/Cases";
 import Contacts from "@/src/components/Contacts";
+import ReviewsSlider from "@/src/components/ReviewsSlider/ReviewsSlider";
 import VideoTestimonials from "@/src/components/VideoTestimonials/VideoTestimonials";
+
 import { Content } from "next/font/google";
 import Image from "next/image";
 
@@ -10,6 +13,8 @@ export default function Home() {
       <Contacts/>
       <Cases/>
       <VideoTestimonials/>
+      {/* <Carousel partners={[]}/> */}
+      <ReviewsSlider/>
     </div>
     
   );
---
+++ src/components/ReviewsSlider/ReviewsSlider.tsx
@@ -0,0 +1,100 @@
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";  // Модуль Navigation
+import "swiper/css"; // Подключаем базовые стили Swiper
+import "swiper/css/navigation"; // Подключаем стили для навигации
+import styles from "./ReviewsSlider.module.scss";
+import Image from "next/image";
+import logoEllipse from "@/public/Ellipse_3092.png"; // Путь к изображению
+import store from '@/public/image/Frame_64.png'
+
+const Reviews = () => {
+  const reviews = [
+    {
+      id: 1,
+      name: "Дмитриев Дмитрий",
+      position: "Директор",
+      company: 'ОсОО "Алматинские конфеты"',
+      logo: logoEllipse,
+      stars: 5,
+      text: `ОсОО "Алматинские конфеты" — "Рахат" выражает искреннюю благодарность коллективу и руководству ОсОО "Болд Брендс Интернетшансы". Мы выражаем нашу глубокую признательность за успешное сотрудничество с вашей компанией.     Ваша профессиональная компетентность, ответственность, оперативность и индивидуальный подход к клиенту привели...`,
+    },
+    {
+        id: 2,
+        name: "Дмитриев Дмитрий",
+        position: "Директор",
+        company: 'ОсОО "Алматинские конфеты"',
+        logo: logoEllipse,
+        stars: 5,
+        text: `ОсОО "Алматинские конфеты" — "Рахат" выражает искреннюю благодарность коллективу и руководству ОсОО "Болд Брендс Интернетшансы". Мы выражаем нашу глубокую признательность за успешное сотрудничество с вашей компанией. Ваша профессиональная компетентность, ответственность, оперативность и индивидуальный подход к клиенту привели...`,
+      },
+  ];
+
+  return (
+    <div className={styles.reviews}>
+        <div className={styles.conteiner}>
+        <div className={styles.textSection}>
+        <h2>Отзывы наших партнеров</h2>
+        <p>Мы гордимся нашими результатами и благодарим наших клиентов за доверие.</p>
+      </div>
+
+      <div className={styles.sliderSection}>
+        <Swiper
+          modules={[Navigation]} 
+          navigation={{ nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" }} // Указываем стрелки
+          spaceBetween={20}
+          slidesPerView={1}
+          className={styles.swiperContainer}
+        >
+          {reviews.map((review) => (
+            <SwiperSlide key={review.id} className={styles.swiperSl}>
+              <div className={styles.reviewCard}>
+              <div className={styles.stars}>
+                    {Array.from({ length: 5 }, (_, index) => (
+                      <Image
+                        key={index}
+                        src={index < review.stars ? store : "/path/to/empty-star.png"}
+                        alt={index < review.stars ? "Full star" : "Empty star"}
+                        className={styles.starImage}
+                        width={20}
+                        height={20}
+                      />
+                    ))}
+                  </div>
+                <div className={styles.blog_name}>
+                 
+                <div className={styles.blog_header}>
+                <Image
+                  src={review.logo}
+                  alt={`${review.company} logo`}
+                  className={styles.logo}
+                />
+                <div className={styles.bog_info}>
+                <h3 className={styles.name}>{review.name}</h3>
+                <p className={styles.position}>{review.position}</p>
+                </div>
+                </div>
+               
+                
+               
+                </div>
+                <p className={styles.text}>{review.text}</p>
+                <a href="#" className={styles.readMore}>
+                  Читать все
+                </a>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        {/* Добавляем стрелки навигации */}
+        <div className="swiper-button-next"></div>
+        <div className="swiper-button-prev"></div>
+      </div>
+        </div>
+      
+    </div>
+  );
+};
+
+export default Reviews;
