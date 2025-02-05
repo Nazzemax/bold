b61440252771b9b86c1ea6247400cc7f9a2dde0b
Nazzemax <hvan1998@inbox.ru>
2025-02-05 21:24:48 +0600

    fix: I fixed filtering cases and added package for debounce

--- src/components/Cases/Cases.tsx
+++ src/components/Cases/Cases.tsx
@@ -1,11 +1,21 @@
 "use client"
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState, useEffect, useCallback } from 'react';
 import styles from './Cases.module.scss';
 import Image from 'next/image';
-import iconBlog from '@/public/image/Frame_41.png';
+import iconBlog from '@/public/caseItem.png';
 import insta from '@/public/image/social.png';
 import tiktok from '@/public/image/tiktok.png';
 import search from '@/public/image/Vector.png';
+import { truncateText } from '@/app/utils/truncateText';
+import { debounce } from 'lodash';
+
+
+const categories = [
+  { id: 1, name: "SMM", active: true },
+  { id: 2, name: "Таргет", active: false },
+  { id: 3, name: "Маркетинг", active: false },
+];
+
 
 const cases = [
   {
@@ -13,7 +23,7 @@ const cases = [
     category: "Корейский ресторан",
     date: "15 Дек. 2023",
     stats: { subscribers: "+10,5К подписчиков", views: "1,5 млн просмотров" },
-    tags: [],
+    tags: ['SMM','Маркетинг'],
     image: iconBlog,
   },
   {
@@ -21,7 +31,7 @@ const cases = [
     category: "Консульт недвижимость",
     date: "15 Дек. 2023",
     stats: { subscribers: "+10,5К подписчиков", views: "1,5 млн просмотров" },
-    tags: [],
+    tags: ['Маркетинг'],
     image: iconBlog,
   },
   {
@@ -29,7 +39,7 @@ const cases = [
     category: "Корейский ресторан",
     date: "15 Дек. 2023",
     stats: { subscribers: "+10,5К подписчиков", views: "1,5 млн просмотров" },
-    tags: [],
+    tags: ['Таргет'],
     image: iconBlog,
   },
   {
@@ -37,7 +47,7 @@ const cases = [
     category: "Консульт недвижимость",
     date: "15 Дек. 2023",
     stats: { subscribers: "+10,5К подписчиков", views: "1,5 млн просмотров" },
-    tags: [],
+    tags: ['SMM','Маркетинг'],   
     image: iconBlog,
   },
   {
@@ -45,7 +55,7 @@ const cases = [
     category: "Корейский ресторан",
     date: "15 Дек. 2023",
     stats: { subscribers: "+10,5К подписчиков", views: "1,5 млн просмотров" },
-    tags: [],
+    tags: ['SMM','Маркетинг'],
     image: iconBlog,
   },
   {
@@ -53,7 +63,7 @@ const cases = [
     category: "Корейский ресторан",
     date: "15 Дек. 2023",
     stats: { subscribers: "+10,5К подписчиков", views: "1,5 млн просмотров" },
-    tags: [],
+    tags: ['SMM','Маркетинг'],
     image: iconBlog,
   },
   {
@@ -61,7 +71,7 @@ const cases = [
     category: "Крупный бизнес",
     date: "20 Янв. 2024",
     stats: { subscribers: "+15К подписчиков", views: "2 млн просмотров" },
-    tags: [],
+    tags: ['SMM','Маркетинг'],
     image: iconBlog,
   },
   {
@@ -69,15 +79,49 @@ const cases = [
     category: "E-commerce",
     date: "18 Янв. 2024",
     stats: { subscribers: "+8К подписчиков", views: "1,2 млн просмотров" },
-    tags: [],
+    tags: ['SMM','Маркетинг'],
     image: iconBlog,
   },
 ];
 
+
+
 function Cases() {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 6; // Устанавливаем количество карточек на странице
-  const totalPages = Math.ceil(cases.length / itemsPerPage);
+
+
+  const [selectedTag, setSelectedTag] = useState('');  
+  const [searchQuery, setSearchQuery] = useState('');
+  const [debouncedSearchQuery, setDebouncedSearchQuery] = useState('');
+
+  const debouncedSearch = useCallback(
+    debounce((searchQuery:string) => {
+      setDebouncedSearchQuery(searchQuery);
+    }, 500),
+    []
+  );
+
+
+
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchQuery(value);
+    debouncedSearch(value); 
+  };
+
+// Используем debouncedSearchQuery вместо searchQuery
+const filteredCases = cases.filter((caseItem) => {
+  const matchesTag = selectedTag ? caseItem.tags.includes(selectedTag) : true;
+  const matchesSearch =
+    debouncedSearchQuery === '' ||
+    caseItem.title.toLowerCase().includes(debouncedSearchQuery.toLowerCase());
+
+  return matchesTag && matchesSearch;
+});
+
+const totalPages = Math.ceil(filteredCases.length / itemsPerPage);
+const shouldShowPagination = totalPages > 1;
 
   const handlePrevPage = () => {
     if (currentPage > 1) {
@@ -91,23 +135,37 @@ function Cases() {
     }
   };
 
-  const displayedCases = cases.slice(
+  const displayedCases = filteredCases.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
   );
 
+  const handleTagClick = (tagName: string) => {
+    if (selectedTag === tagName) {
+      setSelectedTag(''); 
+    } else {
+      setSelectedTag(tagName);
+    }
+  };
+
   return (
     <div className={styles.pageWrapper}>
+        
+      <div className={styles.conteiner}>
       <h1 className={styles.title}>Кейсы</h1>
-      <div className={styles.filters}>
-        <div className={styles.filterButtons}>
-          <button className={styles.filterButton}>SMM</button>
-          <button className={styles.filterButton}>Таргет</button>
-          <button className={styles.filterButton}>Маркетинг</button>
-        </div>
+      <div className={styles.filters}>  
+      <div className={styles.filterButtons}>
+      {categories.map((category, index) => (
+        <button key={index} className={styles.filterButton} onClick={() => handleTagClick(category.name)}>
+          {category.name}
+        </button>
+      ))}
+    </div>
         <div className={styles.searchWrapper}>
-          <input type="text" placeholder="Найти кейс" className={styles.searchInput} />
+          <form >
+          <input type="text" placeholder="Найти кейс" className={styles.searchInput} onChange={handleSearchChange} value={searchQuery}/>
           <Image src={search} alt="" className={styles.searchImage} />
+          </form>
         </div>
       </div>
       <div className={styles.casesGrid}>
@@ -131,7 +189,7 @@ function Cases() {
                 </span>
               </div>
               <div className={styles.caseContent}>
-                <h3 className={styles.caseTitle}>{item.title}</h3>
+                <h3 className={styles.caseTitle}>{truncateText(item.title)}</h3>
                 <div className={styles.caseDetails}>
                   <span>{item.category}</span>
                   <span>{item.date}</span>
@@ -142,23 +200,32 @@ function Cases() {
         ))}
       </div>
       <div className={styles.pagination}>
-        <button
-          className={styles.paginationButton}
-          onClick={handlePrevPage}
-          disabled={currentPage === 1}
-        >
-          &#8249;
-        </button>
-        <span>
-          {String(currentPage).padStart(2, '0')} / {String(totalPages).padStart(2, '0')}
-        </span>
-        <button
-          className={styles.paginationButton}
-          onClick={handleNextPage}
-          disabled={currentPage === totalPages}
-        >
-          &#8250;
-        </button>
+  {/* Кнопка "назад" (отключается на первой странице) */}
+  <button
+    className={styles.paginationButton}
+    onClick={handlePrevPage}
+    disabled={currentPage === 1}
+  >
+    &#8249;
+  </button>
+
+  {/* Отображение текущей страницы / общего количества страниц */}
+  {totalPages > 1 && (
+    <span>
+      {String(currentPage).padStart(2, '0')} / {String(totalPages).padStart(2, '0')}
+    </span>
+  )}
+
+  {/* Кнопка "вперед" (отключается на последней странице) */}
+  <button
+    className={styles.paginationButton}
+    onClick={handleNextPage}
+    disabled={currentPage === totalPages}
+  >
+    &#8250;
+  </button>
+</div>
+
       </div>
     </div>
   );
