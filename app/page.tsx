2ddd8317f8bcd7bc512f519c8faf83b205291fce
Nazzemax <hvan1998@inbox.ru>
2025-02-08 14:25:51 +0600

    fix: removed unnecessary files and added some example usage of component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import Header from "./components/layout/header/Header";
-import DesignSupport from "@/app/components/design-support/DesignSupport";
+import DesignSupport from "@/app/components/businessOffer/BusinessOffers";
+import brain from '@/public/brain.svg'
+import handshake from '@/public/handshake.jpg'
 
 export default function Home() {
   return (
    <>
     <Header bgColor="black"/>
-    <DesignSupport/>
+    <DesignSupport
+    title="Результаты, которые работают на ваш бизнес"
+    rightText="Мы понимаем, что для предпринимателей важны не просто идеи, а измеримые результаты - рост заявок, привлечение клиентов и увеличение прибыли."
+    leftTitle="Наша философия"
+    leftText="Работать для вашего успеха, предлагая стратегии, которые решают именно ваши бизнес-задачи и обеспечивают конкретные достижения."
+    icon={brain}
+    image={handshake}
+    rightTextDown
+    leftTextDown
+   />
    </>
   );
 }
