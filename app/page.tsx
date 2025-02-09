796a34603d91f08ef71880c804e08872398f551e
artemious2077 <malcevartem807@gmail.com>
2025-02-10 00:53:52 +0600

    feat: добавил форму

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-// import Advantages from "./components/layout/advantages/Advantages";
+import Advantages from "./components/layout/advantages/Advantages";
 import Form from "./components/layout/form/Form";
-// import Header from "./components/layout/header/Header";
+import Header from "./components/layout/header/Header";
 
 export default function Home() {
   return (
     <>
-      {/* <Header bgColor="black" /> */}
+      <Header bgColor="black" />
       <Form/>
-      {/* <Advantages/> */}
+      <Advantages/>
     </>
   );
 }
