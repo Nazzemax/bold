21be30b985d63612a31778c0bd12257b28f90fe2
artemious2077 <malcevartem807@gmail.com>
2025-02-11 23:12:13 +0600

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-// import Advantages from "./components/layout/advantages/Advantages";
+import Advantages from "./components/layout/advantages/Advantages";
+import CasesSlider from "./components/layout/cases-slider/CasesSlider";
 import Form from "./components/layout/form/Form";
-// import Header from "./components/layout/header/Header";
 
 export default function Home() {
   return (
     <>
-      {/* <Header bgColor="black" /> */}
-      <Form/>
-      {/* <Advantages/> */}
+      <CasesSlider />
+      <Form />
+      <Advantages />
     </>
   );
 }
