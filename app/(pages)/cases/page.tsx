2286c40313a23ace79d390f4f34441f227dfac19
Nazzemax <hvan1998@inbox.ru>
2025-02-21 00:38:12 +0600

    feat: added 1 block to cases page

--- app/(pages)/cases/page.tsx
+++ app/(pages)/cases/page.tsx
@@ -1,11 +1,16 @@
 
+import { SecondBanner } from '@/widgets/secondBanner/SecondBanner'
 import React from 'react'
+import bgImg from '@/public/banner/img-1.jpg'
 
 type Props = {}
 
 const CasesPage: React.FC<Props> = ({}) => {
      return (
-       <div>CasesPage</div>
+       <div>
+        <SecondBanner bgImg={bgImg} title={'Наша миссия воплощать идеи в жизнь'}
+        />
+       </div>
      )
 }
 
